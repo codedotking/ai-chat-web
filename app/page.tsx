1dcd3c051614e3e86f506b3c6ccc303b1dc74734
techiehe <he.wenyao@nint.com>
2024-06-05 18:43:21 +0800

    fix

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,7 @@
 "use client";
 import PromptInput from "@/components/chat/PromptInput";
 import { useRouter } from "next/navigation";
-import { useImmer } from "use-immer";
+import { v4 as uuidv4 } from "uuid";
 
 export type Message = {
   content: string;
@@ -17,9 +17,10 @@ const message = {
 
 export default function Home() {
   const router = useRouter();
+  uuidv4(); // ⇨ '9b1deb4d-3b7d-4bad-9bdd-2b0d7b3dcb6d'
   const handleSubmit = (prompt: string) => {
     console.log(prompt);
-    router.push(`/chat?prompt=${prompt}`);
+    router.replace(`/chat/${uuidv4()}?prompt=${prompt}`);
   };
 
   return (
--- components/chat/PromptInput.tsx
+++ components/chat/PromptInput.tsx
@@ -2,7 +2,7 @@ import { Button } from "@/components/ui/button";
 import { PaperPlaneIcon } from "@radix-ui/react-icons";
 import { memo, useCallback, useEffect, useMemo, useRef, useState } from "react";
 import { cn } from "@/lib/utils";
-import Textarea from "@/components/TextArea";
+import TextareaAutosize from "react-textarea-autosize";
 interface ComponentProps {
   className?: string;
   onSubmit: (prompt: string) => void;
@@ -17,13 +17,14 @@ const PromptInput = memo<ComponentProps>(function PromptInput({
   className,
   onSubmit,
 }) {
-  const textRef = useRef<string>("");
-  const [enableSubmit, setEnableSubmit] = useState(false);
+  const [prompt, setPrompt] = useState<string>("");
+  const enableSubmit = useMemo(() => {
+    return prompt.trim().length > 0;
+  }, [prompt]);
   const handleSubmit = () => {
-    if (textRef.current.trim().length > 0) {
-      onSubmit(textRef.current);
-      textRef.current = "";
-      setEnableSubmit(false);
+    if (prompt.trim().length > 0) {
+      onSubmit(prompt);
+      setPrompt("");
     }
   };
   return (
@@ -34,18 +35,17 @@ const PromptInput = memo<ComponentProps>(function PromptInput({
       )}>
       {/* 输入框 */}
       <div className="flex-grow overflow-auto break-all flex items-center">
-        <Textarea
-          value={textRef.current}
-          className="overflow-y-auto max-h-32 w-full text-white bg-transparent  border-none focus-visible:outline-none"
+        <TextareaAutosize
+          value={prompt}
+          onChange={(e) => {
+            setPrompt(e.target.value);
+          }}
           onKeyDown={(e) => {
             if (e.key == "Enter" && e.ctrlKey) {
               handleSubmit();
             }
           }}
-          onChange={(pro) => {
-            textRef.current = pro;
-            setEnableSubmit(pro.trim().length > 0);
-          }}
+          className="overflow-y-auto max-h-32 w-full text-white bg-transparent  border-none focus-visible:outline-none"
         />
       </div>
       <div className="flex items-end mr-auto">
